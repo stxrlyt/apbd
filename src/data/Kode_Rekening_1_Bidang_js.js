3814ca8c495d68e78adc453fd95d9595f2a580c8
Piies <melvin170904@gmail.com>
2025-11-25 23:28:45 +0700

    nambahin json kode rekening buat nanti dipake

---
+++ src/data/Kode_Rekening_1_Bidang_js.js
@@ -0,0 +1,2 @@
+// Kode Rekening 1 Bidang
+const Kode_Rekening_1_Bidang = require('./Kode_Rekening_1_Bidang.json');
---
+++ src/data/Kode_Rekening_2_PBP_js.js
@@ -0,0 +1,2 @@
+// Kode Rekening 2 PBP
+export const kodeRekening2PBP = require('./Kode_Rekening_2_PBP.json');
